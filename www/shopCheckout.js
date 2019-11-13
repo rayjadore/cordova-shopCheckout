e87c228df80dca5e6cc0ce012fc0ea8c9dc2849e
rayjadore <conwuegbuzie@tradedepot.co>
2019-11-13 13:01:33 +0100

    --

--- www/shopCheckout.js
+++ www/shopCheckout.js
@@ -2,19 +2,19 @@ var exec = require('cordova/exec');
 
 var shopCheckout = {
     registerAgent: function(options, success, error) {
-        exec(success, error, 'shopCheckout', 'registerAgent', [options]);
+        exec(success, error, 'ShopCheckout', 'registerAgent', [options]);
     },
 
     openCart: function(options, success, error) {
-        exec(success, error, 'shopCheckout', 'openCart', []);
+        exec(success, error, 'ShopCheckout', 'openCart', []);
     },
 
     openTransactions: function(success, error) {
-        exec(success, error, 'shopCheckout', 'openTransactions', []);
+        exec(success, error, 'ShopCheckout', 'openTransactions', []);
     },
 
     logout: function(success, error) {
-        exec(success, error, 'shopCheckout', 'logout', []);
+        exec(success, error, 'ShopCheckout', 'logout', []);
     }
 
 }
